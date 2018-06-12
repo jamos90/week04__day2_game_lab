ba6ad8b5c37e23429931bea5e8f31c8d25631f2b
jamos90 <jamos_98@hotmail.com>
2018-06-12 16:44:53 +0100

    description made

--- contoller.rb
+++ contoller.rb
@@ -4,6 +4,15 @@ require("sinatra/contrib/all")
 require_relative('./models/rock_paper_scissors.rb')
 also_reload('./models/*')
 
+
+get '/' do
+  erb(:home)
+end
+
+get '/about_game' do
+  erb(:about_game)
+end
+
 get '/rock_wins/:move1/:move2' do
   game = Game.new(params[:move1], params[:move2])
   @game = game.play_game()
